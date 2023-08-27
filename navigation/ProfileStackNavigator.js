583a497ffdb8a9113553bbd6e7eadd759b29d064
linbrittany <brlin@itba.edu.ar>
2023-08-27 18:14:55 -0300

    feat: pop up menu on profile

--- navigation/ProfileStackNavigator.js
+++ navigation/ProfileStackNavigator.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import Profile from '../screens/Profile';
-import MyEvents from '../screens/MyEvents';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Profile from "../screens/Profile";
+import MyEvents from "../screens/MyEvents";
 
 const Stack = createNativeStackNavigator();
 
 const ProfileStackNavigator = () => {
-    return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Group>
-                <Stack.Screen name="MyProfile" component={Profile}/>
-                {/* TODO: britu */}
-                {/* <Stack.Screen name="EditProfile" component={EditProfile}/> */}
-            </Stack.Group>
-        </Stack.Navigator>
-    );
-}
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Group>
+        <Stack.Screen
+          
+          name="MyProfile"
+          component={Profile}
+        />
+
+        {/* TODO: britu */}
+        {/* <Stack.Screen name="EditProfile" component={EditProfile}/> */}
+      </Stack.Group>
+    </Stack.Navigator>
+  );
+};
 
 export default ProfileStackNavigator;
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,45 +1,84 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import HomeStackNavigator from './HomeStackNavigator';
-import ProfileStackNavigator from './ProfileStackNavigator'
-import { COLORS } from '../constants';
-import { MyEvents } from '../screens';
-import { useNavigation } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import HomeStackNavigator from "./HomeStackNavigator";
+import ProfileStackNavigator from "./ProfileStackNavigator";
+import { COLORS } from "../constants";
+import { MyEvents } from "../screens";
+import { useNavigation } from "@react-navigation/native";
+import { Menu, MenuItem, MenuDivider } from "react-native-material-menu";
+import { TouchableOpacity } from "react-native";
+import { StatusBar } from "react-native";
+import React, { useState } from "react";
 const Tab = createBottomTabNavigator();
-
 
 const Tabs = () => {
   const navigator = useNavigation();
+  const [visible, setVisible] = useState(false);
+
   return (
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarActiveTintColor: COLORS.primary,
-          tabBarInactiveTintColor: 'gray',
-          tabBarIcon: ({ color, size }) => {
-            let iconName;
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: "gray",
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
 
-            if (route.name === 'HomeNavigator') {
-              iconName = 'home';
-            } else if (route.name === 'Perfil') {
-              iconName = 'person';
-            } else if (route.name === 'Mis Eventos') {
-              iconName = 'calendar'
-            }
-            return <Ionicons name={iconName} size={size} color={color} />;
+          if (route.name === "HomeNavigator") {
+            iconName = "home";
+          } else if (route.name === "Perfil") {
+            iconName = "person";
+          } else if (route.name === "Mis Eventos") {
+            iconName = "calendar";
           }
-        })}
-      >
-        <Tab.Screen
-          options={{
-            headerShown: false,
-            title: 'Inicio'
-          }}
-          name="HomeNavigator" component={HomeStackNavigator} />
-        <Tab.Screen name="Mis Eventos" component={MyEvents} />
-        <Tab.Screen name="Perfil" component={ProfileStackNavigator} />
-      </Tab.Navigator>
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen
+        options={{
+          headerShown: false,
+          title: "Inicio",
+        }}
+        name="HomeNavigator"
+        component={HomeStackNavigator}
+      />
+      <Tab.Screen name="Mis Eventos" component={MyEvents} />
+      <Tab.Screen
+        options={{
+          headerRight: () => {
+            return (
+              <Menu
+                visible={visible}
+                anchor={
+                  <TouchableOpacity onPress={() => setVisible(!visible)}>
+                    <Ionicons
+                      name="menu"
+                      style={{ marginRight: 10, marginTop: 1 }}
+                      size={24}
+                      color={COLORS.primary}
+                    />
+                  </TouchableOpacity>
+                }
+                onRequestClose={() => setVisible(!visible)}
+              >
+                <MenuItem
+                  onPress={() => {
+                    console.log("Edit Profile");
+                    setVisible(!visible);
+                  }}
+                >
+                  Edit Profile
+                </MenuItem>
+                <MenuDivider />
+                <MenuItem onPress={() => setVisible(!visible)}>Logout</MenuItem>
+              </Menu>
+            );
+          },
+        }}
+        name="Perfil"
+        component={ProfileStackNavigator}
+      />
+    </Tab.Navigator>
   );
-}
-
+};
 export default Tabs;
