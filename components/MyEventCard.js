561e19354e54a007dcdda9016ef2806ae0e3d6d1
Agustin Naso <anaso@itba.edu.ar>
2023-09-15 16:45:29 -0300

    feat: basic rate

--- components/MyEventCard.js
+++ components/MyEventCard.js
@@ -1,19 +1,21 @@
 import React, { useEffect } from "react"
-import { Linking, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { Linking, Modal, Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { Ionicons } from '@expo/vector-icons';
 import { acceptParticipant, fetchUser } from "../services/eventService";
-import { set } from "react-hook-form";
-import { Avatar, Button } from "@rneui/base";
+import { Avatar, Button, color } from "@rneui/base";
 import { COLORS } from "../constants";
+import { Rating, RatingProps } from '@rneui/themed';
+
 
 
 const MyEventCard = ({ userData }) => {
-    console.log("USER DATA", userData)
 
     const [userAccepted, setUserAccepted] = React.useState(userData.participant_status == "true");
+    const [modalVisible, setModalVisible] = React.useState(false);
+
 
     const openRateModal = () => {
-
+        setModalVisible(true);
     }
 
 
@@ -34,6 +36,23 @@ const MyEventCard = ({ userData }) => {
 
     return (
         <View style={styles.card}>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Puntua al participante</Text>
+                        <View style={{marginBottom: 10}}>
+                            <Rating showRating imageSize={20} fractions={1} onFinishRating={rating => console.log(rating)} />
+                        </View>
+                        <Button color={COLORS.primary} mode="contained" title="Guardar puntuacion" onPress={() => setModalVisible(false)} />
+                    </View>
+                </View>
+            </Modal>
             <View style={styles.textContainer}>
                 <Avatar rounded size={62} source={require("../assets/default-profile.png")} containerStyle={{ backgroundColor: COLORS.secondary }} />
                 <Text style={styles.userText}>{userData.firstname}</Text>
@@ -50,7 +69,7 @@ const MyEventCard = ({ userData }) => {
                 : userData.eventStatus !== 2 ?
                     <Button color={COLORS.primary} mode="contained" title="Contactar" onPress={sendMessage} />
                     : userData.eventStatus === 2 && <Button color={COLORS.primary} mode="contained" title="Puntuar" onPress={openRateModal} />
-                }
+            }
 
         </View>
     );
@@ -88,5 +107,46 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: 600,
         marginLeft: 12
-    }
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 16,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 2,
+        elevation: 5,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
 });
