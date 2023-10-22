093c301ba20452b091c5cfcaae0601495440167c
linbrittany <brlin@itba.edu.ar>
2023-10-22 17:40:35 -0300

    fix: if no use image return null

--- services/userService.js
+++ services/userService.js
@@ -75,8 +75,8 @@ export const fetchUserImage = async (userId) => {
 
     return {
       status: response.status,
-      imageURL: `data:image/png;base64,${data}`
-    }
+      imageURL: data ? `data:image/png;base64,${data}` : null,
+    };
   }
 
   return {
