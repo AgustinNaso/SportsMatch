964333b2b9f05181c0ad6ffbf8bc87ca1e8653fb
linbrittany <brlin@itba.edu.ar>
2023-11-10 16:16:13 -0300

    fix: call fetchUser once on MyEvents

--- screens/MyEvents.js
+++ screens/MyEvents.js
@@ -1,106 +1,122 @@
-import React, { useEffect } from 'react';
-import { Text, SafeAreaView, FlatList, View } from 'react-native';
-import { TabView, TabBar } from 'react-native-tab-view';
-import Card from '../components/Card';
-import { fetchJoinedEvents, fetchMyEvents, fetchNearEvents, fetchUser, removeParticipant } from '../services/eventService';
-import { getCurrentUserData } from '../services/authService';
-import { COLORS } from '../constants';
-import { useIsFocused } from '@react-navigation/native';
-import MyEventList from '../components/MyEventList';
+import React, { useEffect } from "react";
+import { Text, SafeAreaView, FlatList, View } from "react-native";
+import { TabView, TabBar } from "react-native-tab-view";
+import Card from "../components/Card";
+import {
+  fetchJoinedEvents,
+  fetchMyEvents,
+  fetchNearEvents,
+  fetchUser,
+  removeParticipant,
+} from "../services/eventService";
+import { getCurrentUserData } from "../services/authService";
+import { COLORS } from "../constants";
+import { useIsFocused } from "@react-navigation/native";
+import MyEventList from "../components/MyEventList";
 
 const renderList = (data) => {
-    return <MyEventList data={data} />
-}
+  return <MyEventList data={data} />;
+};
 
 const renderJoinedItem = ({ item }) => {
-    return <Card props={item} />
-}
+  return <Card props={item} />;
+};
 
 const FirstRoute = (myEvents) => (
-    <SafeAreaView style={{ flex: 1 }}>
-        <FlatList
-            data={myEvents} renderItem={(data) => renderList(data)}
-            style={{ flex: 1 }} keyExtractor={(item, index) => {
-                return `${index}`
-            }}>
-        </FlatList>
-    </SafeAreaView>
+  <SafeAreaView style={{ flex: 1 }}>
+    <FlatList
+      data={myEvents}
+      renderItem={(data) => renderList(data)}
+      style={{ flex: 1 }}
+      keyExtractor={(item, index) => {
+        return `${index}`;
+      }}
+    ></FlatList>
+  </SafeAreaView>
 );
 
 const SecondRoute = (joinedEvents) => (
-    <SafeAreaView style={{ flex: 1 }}>
-        <FlatList
-            data={joinedEvents} renderItem={renderJoinedItem}
-            contentContainerStyle={{ flexGrow: 1 }}
-            style={{ flex: 1 }} keyExtractor={(item, index) => `${item.event_id}-${index}`}
-            ListEmptyComponent={renderEmptyList}>
-        </FlatList>
-    </SafeAreaView>
+  <SafeAreaView style={{ flex: 1 }}>
+    <FlatList
+      data={joinedEvents}
+      renderItem={renderJoinedItem}
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={{ flex: 1 }}
+      keyExtractor={(item, index) => `${item.event_id}-${index}`}
+      ListEmptyComponent={renderEmptyList}
+    ></FlatList>
+  </SafeAreaView>
 );
 
 const renderEmptyList = () => {
-    return (
-        <View style={{flex: 1, justifyContent: 'center', height: '100%'}}>
-            <Text style={{ fontSize: 20, alignSelf: 'center' }}>No hay partidos por jugar</Text>
-        </View>
-    )
-}
+  return (
+    <View style={{ flex: 1, justifyContent: "center", height: "100%" }}>
+      <Text style={{ fontSize: 20, alignSelf: "center" }}>
+        No hay partidos por jugar
+      </Text>
+    </View>
+  );
+};
 
 const MyEvents = () => {
-    const [index, setIndex] = React.useState(0);
-    const [routes] = React.useState([
-        { key: 'first', title: 'Creados' },
-        { key: 'second', title: 'Anotado' },
-    ]);
-    const [myEvents, setMyEvents] = React.useState([]);
-    const [joinedEvents, setJoinedEvents] = React.useState([]);
-    const [user, setUser] = React.useState(null);
-    const isFocused = useIsFocused();
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "Creados" },
+    { key: "second", title: "Anotado" },
+  ]);
+  const [myEvents, setMyEvents] = React.useState([]);
+  const [joinedEvents, setJoinedEvents] = React.useState([]);
+  const [user, setUser] = React.useState(null);
+  const isFocused = useIsFocused();
 
-    const renderScene = ({ route }) => {
-        switch (route.key) {
-            case 'first':
-                return myEvents.length > 0 ? FirstRoute(myEvents) : null
-            case 'second':
-                return SecondRoute(joinedEvents)
-            default:
-                return null;
-        }
+  const renderScene = ({ route }) => {
+    switch (route.key) {
+      case "first":
+        return myEvents.length > 0 ? FirstRoute(myEvents) : null;
+      case "second":
+        return SecondRoute(joinedEvents);
+      default:
+        return null;
+    }
+  };
+
+  useEffect(() => {
+    getCurrentUserData().then(async (data) => {
+      const userData = await fetchUser(data.email);
+      setUser(userData);
+    });
+  }, []);
+
+  useEffect(() => {
+    const getMyEvents = async () => {
+      const data = await fetchMyEvents(user.userid);
+      setMyEvents(data.items);
     };
+    const getJoinedEvents = async () => {
+      const mockData = await fetchJoinedEvents(user.userid);
+      setJoinedEvents(mockData.items);
+    };
+    if (user && isFocused) {
+      getMyEvents().catch((err) => console.log(err));
+      getJoinedEvents().catch((err) => console.log(err));
+    }
+  }, [user, isFocused]);
 
-    useEffect(() => {
-        getCurrentUserData().then((data) => {
-            setUser(data);
-        });
-
-    }, [])
-
-    useEffect(() => {
-        const getMyEvents = async () => {
-            const userData = await fetchUser(user.email);
-            const data = await fetchMyEvents(userData.user_id);
-                        setMyEvents(data.items);
-        }
-        const getJoinedEvents = async () => {
-            const userData = await fetchUser(user.email);
-            const mockData = await fetchJoinedEvents(userData?.user_id);
-            setJoinedEvents(mockData.items);
-        }
-        if (isFocused) {
-            getMyEvents().catch(err => console.log(err));
-            getJoinedEvents().catch(err => console.log(err));
-        }
-    }, [user, isFocused]);
-
-    return ( 
-        <TabView
-            renderTabBar={(props) => <TabBar {...props} style={{ backgroundColor: COLORS.primary }} indicatorStyle={{ backgroundColor: COLORS.secondary }} />}
-            navigationState={{ index, routes }}
-            renderScene={renderScene}
-            onIndexChange={setIndex}
-            initialLayout={{ width: 100 }}
+  return (
+    <TabView
+      renderTabBar={(props) => (
+        <TabBar
+          {...props}
+          style={{ backgroundColor: COLORS.primary }}
+          indicatorStyle={{ backgroundColor: COLORS.secondary }}
         />
-    );
-}
+      )}
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: 100 }}
+    />
+  );
+};
 
 export default MyEvents;
--- services/eventService.js
+++ services/eventService.js
@@ -1,177 +1,180 @@
 import { EVENT_STATUS, EXPERTISE } from "../constants/data";
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from "expo-secure-store";
 
-
-export const API_URL = 'http://sportsmatch-lb-700737557.us-east-1.elb.amazonaws.com'
+export const API_URL =
+  "http://sportsmatch-lb-700737557.us-east-1.elb.amazonaws.com";
 
 export const authenticatedFetch = async (url, options = {}) => {
-    try {
-        const token = await SecureStore.getItemAsync('userToken');
-        const headers = {
-            ...options.headers,
-            'C-api-key': token
-        };
-        const response = await fetch(API_URL + url, { ...options, headers });
-        console.log(`Response for ${url} :`, response.status);
-        if (response.status >= 400 && response.status < 600) {
-            const body = await response.json();
-            if (response.status === 401 && body.internalStatus === "TOKEN_EXPIRED") {
-                // await refreshSession();
-                // return await authenticatedFetch(url, options);
-            }
-            else
-                throw new Error("Bad response from server");
-        }
-        return response;
-    } catch (err) {
-        console.log('ERROR: ', err);
+  try {
+    const token = await SecureStore.getItemAsync("userToken");
+    const headers = {
+      ...options.headers,
+      "C-api-key": token,
+    };
+    const response = await fetch(API_URL + url, { ...options, headers });
+    console.log(`Response for ${url} :`, response.status);
+    if (response.status >= 400 && response.status < 600) {
+      const body = await response.json();
+      if (response.status === 401 && body.internalStatus === "TOKEN_EXPIRED") {
+        // await refreshSession();
+        // return await authenticatedFetch(url, options);
+      } else throw new Error("Bad response from server");
     }
-}
+    return response;
+  } catch (err) {
+    console.log("ERROR: ", err);
+  }
+};
 
 export const fetchUser = async (email) => {
-    const data = await fetch(API_URL + '/users?email=' + email);
-    const json = await data.json();
-    return json;
-}
-
+  const data = await fetch(API_URL + "/users?email=" + email);
+  const json = await data.json();
+  return json;
+};
 
 export const fetchParticipants = async (eventId) => {
-    const response = await fetch(API_URL + '/events/' + eventId + '/owner/participants');
-    const json = await response.json();
-    return json;
-}
+  const response = await fetch(
+    API_URL + "/events/" + eventId + "/owner/participants"
+  );
+  const json = await response.json();
+  return json;
+};
 
 //TODO: clean this code
 export const fetchEvents = async (filters) => {
-    const data = JSON.parse(await SecureStore.getItemAsync('userPayload'));
-    console.log("FILTROS ", filters)
-    let filterString;
-    if (filters) {
-        if (filters.date)
-            filters.date = filters.date.split("T")[0];
-        else
-            delete filters.date;
-        if (filters.expertise)
-            filters.expertise = EXPERTISE.indexOf(filters.expertise);
-        else
-            delete filters.expertise;
-        if (!filters.schedule || filters.schedule.length == 0)
-            delete filters.schedule;
-        filterString = Object.entries(filters).map(([key, value]) => {
-            console.log("KEY: " + key + " VALUE: " + value)
-            return "&" + key + "=" + value;
-        }).join("");
-
-    }
-    return await fetch(`${API_URL}/events?userId=${data.user_id}&filterOut=true${filterString ?? ""}&limit=200`);
-}
+  const data = JSON.parse(await SecureStore.getItemAsync("userPayload"));
+  console.log("FILTROS ", filters);
+  let filterString;
+  if (filters) {
+    if (filters.date) filters.date = filters.date.split("T")[0];
+    else delete filters.date;
+    if (filters.expertise)
+      filters.expertise = EXPERTISE.indexOf(filters.expertise);
+    else delete filters.expertise;
+    if (!filters.schedule || filters.schedule.length == 0)
+      delete filters.schedule;
+    filterString = Object.entries(filters)
+      .map(([key, value]) => {
+        console.log("KEY: " + key + " VALUE: " + value);
+        return "&" + key + "=" + value;
+      })
+      .join("");
+  }
+  return await fetch(
+    `${API_URL}/events?userId=${data.user_id}&filterOut=true${
+      filterString ?? ""
+    }&limit=200`
+  );
+};
 
 export const fetchJoinedEvents = async (userId) => {
-    const response = await fetch(API_URL + '/events?participantId=' + userId);
-    const jsonRes = await response.json();
-    return jsonRes;
-}
+  const response = await fetch(API_URL + "/events?participantId=" + userId);
+  const jsonRes = await response.json();
+  return jsonRes;
+};
 
-export const fetchMyEvents = async (uid) => {
-    const data = await SecureStore.getItemAsync('userPayload');
-    const userData = await fetchUser(JSON.parse(data).email);
-    const events = await fetch(API_URL + `/events?userId=${userData.user_id}`);
-    const json = await events.json();
-    const response = json;
+export const fetchMyEvents = async (userId) => {
+  const events = await fetch(API_URL + `/events?userId=${userId}`);
+  const json = await events.json();
+  const response = json;
 
-    console.log("MIS EVENTOS : ", response.items)
-    for (let i = 0; i < response.items.length; i++)
-        response.items[i].participants = await fetchParticipants(response.items[i].event_id);
+  console.log("MIS EVENTOS : ", response.items);
+  for (let i = 0; i < response.items.length; i++)
+    response.items[i].participants = await fetchParticipants(
+      response.items[i].event_id
+    );
 
-    return response;
-}
+  return response;
+};
 
 export const fetchNearEvents = async (filters = undefined) => {
-    try {
-        const data = await SecureStore.getItemAsync('userPayload');
-        const response = await fetchEvents(filters);
-        console.log("RESPONSE: ", response.status)
-        let jsonRes = await response.json();
-        console.log("EVENTOS CERCANOS: ", jsonRes.items);
-        jsonRes.items = jsonRes.items?.filter(event => event.remaining > 0 && event.event_status !== EVENT_STATUS.FINALIZED);
-        return jsonRes
-    }
-    catch (err) {
-        console.log("ERRPR", err);
-    }
-}
+  try {
+    const data = await SecureStore.getItemAsync("userPayload");
+    const response = await fetchEvents(filters);
+    console.log("RESPONSE: ", response.status);
+    let jsonRes = await response.json();
+    console.log("EVENTOS CERCANOS: ", jsonRes.items);
+    jsonRes.items = jsonRes.items?.filter(
+      (event) =>
+        event.remaining > 0 && event.event_status !== EVENT_STATUS.FINALIZED
+    );
+    return jsonRes;
+  } catch (err) {
+    console.log("ERRPR", err);
+  }
+};
 
 export const publishEvent = async (eventData) => {
-    await authenticatedFetch('/events', {
-        method: 'POST',
-        body: JSON.stringify(eventData),
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-}
+  await authenticatedFetch("/events", {
+    method: "POST",
+    body: JSON.stringify(eventData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 
 export const fetchUserId = async (email, userJWT) => {
-    const data = await fetch(API_URL + '/users?email=' + email);
-    return await data.json();
-}
+  const data = await fetch(API_URL + "/users?email=" + email);
+  return await data.json();
+};
 
 export const fetchEventById = async (eventId) => {
-    const data = await fetch(API_URL + '/events/' + eventId);
-    return await data.json();
-}
+  const data = await fetch(API_URL + "/events/" + eventId);
+  return await data.json();
+};
 
 export const joinNewEvent = async (eventId, userId) => {
-    const data = await SecureStore.getItemAsync('userPayload');
-    const userData = await fetchUser(JSON.parse(data).email);
+  const data = await SecureStore.getItemAsync("userPayload");
+  const userData = await fetchUser(JSON.parse(data).email);
 
-    await authenticatedFetch('/events/' + eventId + '/participants', {
-        method: 'PUT',
-        body: JSON.stringify({ userId: userData.user_id }),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-}
+  await authenticatedFetch("/events/" + eventId + "/participants", {
+    method: "PUT",
+    body: JSON.stringify({ userId: userData.user_id }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 
 export const acceptParticipant = async (eventId, email) => {
-    await authenticatedFetch('/events/' + eventId + '/owner/participants', {
-        method: 'PUT',
-        body: JSON.stringify({ email: email }),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-}
+  await authenticatedFetch("/events/" + eventId + "/owner/participants", {
+    method: "PUT",
+    body: JSON.stringify({ email: email }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 //Endpoint should be /rating for it to be RESTful
 export const rateUser = async (eventId, rating, participantId) => {
-    await authenticatedFetch(`/users/${participantId}/rate`, {
-        method: 'POST',
-        body: JSON.stringify({ eventId: eventId, rating: rating }),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-}
+  await authenticatedFetch(`/users/${participantId}/rate`, {
+    method: "POST",
+    body: JSON.stringify({ eventId: eventId, rating: rating }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 
 export const removeParticipant = async (eventId) => {
-    console.log("REMOVING MYSELF FROM EVENT: " + eventId);
-    const res = await authenticatedFetch('/events/' + eventId + '/participants', {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-    return res;
-}
+  console.log("REMOVING MYSELF FROM EVENT: " + eventId);
+  const res = await authenticatedFetch("/events/" + eventId + "/participants", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return res;
+};
 
 export const removeParticipantAsOwner = async (eventId, userEmail) => {
-    console.log("REMOVING PARTICIPANT FROM EVENT: " + eventId);
-    await authenticatedFetch('/events/' + eventId + '/owner/participants', {
-        method: 'DELETE',
-        body: JSON.stringify({ email: userEmail }),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-}
+  console.log("REMOVING PARTICIPANT FROM EVENT: " + eventId);
+  await authenticatedFetch("/events/" + eventId + "/owner/participants", {
+    method: "DELETE",
+    body: JSON.stringify({ email: userEmail }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
