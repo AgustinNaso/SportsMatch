cb1a1d13618ade4e45156ab79758765c575a079a
Agustin Naso <anaso@itba.edu.ar>
2023-11-27 02:30:31 -0300

    feat: edit profile title

--- navigation/ProfileStackNavigator.js
+++ navigation/ProfileStackNavigator.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Profile from "../screens/Profile";
 import EditProfile from "../screens/EditProfile";
 import { COLORS } from "../constants";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
 const Stack = createNativeStackNavigator();
 
 const ProfileStackNavigator = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('state', () => {
+      const routeName = navigation.getState().routes[2].state.index === 1 ? 'Edit Profile' : 'MyProfile';
+      console.log(routeName);
+
+      if (routeName === 'Edit Profile') {
+        navigation.setOptions({
+          headerTitle: 'Editar perfil',
+        });
+      } else {
+        navigation.setOptions({
+          headerTitle: 'Perfil',
+        });
+      }
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false, statusBarColor: COLORS.primary }}>
       <Stack.Group>
