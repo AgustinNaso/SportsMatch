dd39b781549de0c9f390f02c88928df19fe9324f
Agustin Naso <anaso@itba.edu.ar>
2023-12-09 00:28:43 -0300

    feat: full locations ( CABA)

--- constants/data.js
+++ constants/data.js
@@ -5,15 +5,21 @@ export const SPORT = ['Fútbol', 'Basquet', 'Paddle', 'Voley', 'Tenis','Ping Pon
 export const MONTHS = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
     "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]//TODO: move to constants
 export const LOCATIONS = [
-         "Agronomía" ,
-         "Almagro" ,
-         "Balvanera" ,
-         "Barracas" ,
-         "Belgrano" ,
-         "Boedo" ,
-         "Caballito" ,
-         "Chacarita" ,
-         "Coghlan" ,
-         "Colegiales"
-    ];
+    'Chacarita',         'Paternal',          'Villa Crespo',
+    'Villa Del Parque',  'Almagro',           'Caballito',
+    'Villa Santa Rita',  'Monte Castro',      'Villa Real',
+    'Flores',            'Floresta',          'Constitucion',
+    'San Cristobal',     'Boedo',             'Velez Sarsfield',
+    'Villa Luro',        'Parque Patricios',  'Mataderos',
+    'Villa Lugano',      'San Telmo',         'Saavedra',
+    'Coghlan',           'Villa Urquiza',     'Colegiales',
+    'Balvanera',         'Villa Gral. Mitre', 'Parque Chas',
+    'Agronomia',         'Villa Ortuzar',     'Barracas',
+    'Parque Avellaneda', 'Parque Chacabuco',  'Nueva Pompeya',
+    'Palermo',           'Villa Riachuelo',   'Villa Soldati',
+    'Villa Pueyrredon',  'Villa Devoto',      'Liniers',
+    'Versalles',         'Puerto Madero',     'Monserrat',
+    'San Nicolas',       'Belgrano',          'Recoleta',
+    'Retiro',            'Nuñez',             'La Boca'
+  ]  
 export const HORARIOS = ["Mañana", "Tarde", "Noche"]
